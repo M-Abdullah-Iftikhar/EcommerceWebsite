33de9e0b04cb759a421f8bedc5de9f20395e17fc
Ameer Hamza <hamzanaseer496@gmail.com>
2024-08-24 16:23:28 +0500

    Update ShopCategory.jsx

--- client/src/Pages/ShopCategory.jsx
+++ client/src/Pages/ShopCategory.jsx
@@ -22,7 +22,7 @@ const ShopCategory = (props) => {
   return (
     <section>
       {loading ? (
-        <div className="flex items-center justify-center min-h-screen">
+        <div className="flex items-center justify-center my-20 md:my-0 md:min-h-screen">
       <div className="relative">
         <div className="h-10 w-10 rounded-full border-t-8 border-b-8 border-yellow-400"></div>
         <div className="absolute top-0 left-0 h-10 w-10 rounded-full border-t-8 border-b-8 border-red-600 animate-spin"></div>
